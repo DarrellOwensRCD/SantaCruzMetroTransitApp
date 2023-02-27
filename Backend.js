fbd32b1290e1fbd3458eb6236d8ae4168a22e563
CamrenColeman <123282989+CamrenColeman@users.noreply.github.com>
2023-02-27 14:51:42 -0800

    updated backend

--- Backend.js
+++ Backend.js
@@ -7,47 +7,53 @@ const backedport = 3000;
 const {spawn} = require('child_process')
 const fs = require('fs');
 
-//https://coderrocketfuel.com/article/handle-get-request-query-string-parameters-in-express-js
 //https://expressjs.com/en/guide/routing.html
+//swagger-UI-Express
+
 const ioport = 5000;
 const { Server } = require("socket.io");
 const io = new Server(ioport, { /* options */ });
 
 app.get('/', (req, resp) => {
     console.log('Incomming request')
-    resp.send('<h1></h1>')
+    //Send list of lines here
 })
 
-// Request for route list
-app.get('/routes', (req, res) => {
-    console.log('Incomming request for list of routes.')
-    //Pull all info, send it
-    // spawn new child process to call the python script
-    const python = spawn('python3', ['Test.py'])
-    io.once("connection", (socket) => {
-        console.log('Connected.')
-        socket.once("Ready", (output) => {
-            socket.emit("sendRequest", { line: '71', stop: 'Front & Soquel Ave', direction: 'outbound' })
-        });
-        // ... 
-        socket.once("listResponse", (output) => {
-            console.log('GotResponse.')
-            res.json(output)
-            python.kill()
-        });
-    }) 
+// Request for line list
+app.get('/line', (req, res) => {
+    console.log('Incomming request for list of lines.')
+    const req_line = req.query.lineNum
+    if(Object.keys(req.query).length === 0){
+        //if req_line is null send list
+    }
+    else{
+        console.log(req_line)
+    }
+    //If not, send line with stops
 })
 
+// Request for stop list
 app.get('/stop', (req, res) => {
     console.log('Incomming request for list of routes.')
-    const req_line = req.query.line
+    const req_stop = req.query.stopNum 
+    if(Object.keys(req.query).length === 0){
+        console.log('Empty.')
+    }
+    //if req_stop is null send list
+    //If not, send stop with lines
+})
+
+app.get('/linestop', (req, res) => {
+    console.log('Incomming request for list of routes.')
+    const req_line = req.query.lineNum
+    const req_stop = req.query.stopID
     const req_dir = req.query.direction || "outbound"
     // spawn new child process to call the python script
-    const python = spawn('python3', ['Test.py'])
+    const python = spawn('python3', ['Testscraper.py'])
     io.once("connection", (socket) => {
         console.log('Connected.')
         socket.once("Ready", (output) => {
-            socket.emit("sendRequest", { line: req_line, stop: 'Front & Soquel Ave', direction: req_dir })
+            socket.emit("sendRequest", { line: req_line, stop: req_stop, direction: req_dir })
         });
         // ... 
         socket.once("listResponse", (output) => {
