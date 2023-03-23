d0391311e70f7bfb820a37944bfbb1dac1795a8c
dhsieh4 <dhsieh4@ucsc.edu>
2023-03-23 11:39:04 -0700

    Formatting

--- LoopProcessor.py
+++ LoopProcessor.py
@@ -3,6 +3,8 @@ import math
 from datetime import datetime, timedelta
 from dateutil import parser
 
+# Opening and Formatting Input files
+
 with open('./current_loops.txt') as infile:
     data = infile.read()
 
@@ -17,9 +19,13 @@ with open('./StopCoordList.txt') as cdfile:
 stops = json.loads(coords)
 
 with open('./StopCoordList.txt','w') as outf:
-    json.dump(stops,outf, indent=4)
+    json.dump(stops, outf, indent=4)
+
+# Initialize response dictionary
 
 response = []
+
+# Search valid buses to create a response for
 
 for bus in buses:
     if (bus["route"] != "OUT OF SERVICE/SORRY") and (bus["route"] != "LOOP OUT OF SERVICE AT BARN THEATER") and (bus["id"] != "90"):
@@ -27,6 +33,9 @@ for bus in buses:
         closest = -1
         eta = 0
         nextBusTime = "error"
+
+        # Search for the closest stop by proximity
+
         for stop in stops:
             #print("Last Latitude:", lastLatitude,"Last Longitude:", lastLongitude, "Stop Latitude:",stop['latitude'],"Stop Longitude:",stop['longitude'])
             distance = math.dist([bus["lastLatitude"],bus["lastLongitude"]],[stop['latitude'],stop['longitude']])
@@ -41,5 +50,7 @@ for bus in buses:
                 nextBusTime = parser.parse(bus["lastPing"]) + timedelta(minutes=eta)
         response.append({"Bus ID":bus["id"], "stopName":closest_stop, "direction":direction, "ETA":eta, "nextBusTime":nextBusTime})
 
+# Print and format list of loop ETAs to output file
+
 with open('./loopETAs.txt','w') as outfile:
-    json.dump(response,outfile,indent=4, sort_keys=True, default=str)
+    json.dump(response, outfile, indent=4, sort_keys=True, default=str)
